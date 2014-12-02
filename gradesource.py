189af3273fe669656a6115e2d339f3f77a5a9ec7
James Kwan <jmkwan@ucsd.edu>
2014-12-02 09:10:30 -0800

    Add pushover message priority support

--- gradesource.py
+++ gradesource.py
@@ -9,7 +9,7 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin, urlencode
 from urllib.request import urlopen
 
-def push_alert(msg, pushover_app, pushover_user):
+def push_alert(msg, pushover_app, pushover_user, priority=-1):
     try:
         conn = http.client.HTTPSConnection("api.pushover.net:443")
         logging.info("Pushover: %s", msg)
@@ -17,6 +17,7 @@ def push_alert(msg, pushover_app, pushover_user):
             'token': pushover_app,
             'user': pushover_user,
             'message': msg,
+            'priority': priority,
         }), {'Content-type': 'application/x-www-form-urlencoded'})
         return conn.getresponse()
     except:
@@ -142,5 +143,5 @@ if __name__ == '__main__':
                 msg = "%s: %s" % (course, notices)
                 print(msg)
                 if 'pushover' in config:
-                    push_alert(msg, config['pushover']['app'], config['pushover']['user'])
+                    push_alert(msg, config['pushover']['app'], config['pushover']['user'], config['pushover']['priority'])
             time.sleep(config['wait']/len(mon))
